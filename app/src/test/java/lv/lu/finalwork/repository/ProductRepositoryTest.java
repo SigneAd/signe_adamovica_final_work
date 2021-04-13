fc4303aa1cd9b816a3744e597da02eb8ea5a4ab3
Signe Adamovica <signe.adamovica@gmail.com>
2021-04-13 19:18:09 +0300

    test added

---
+++ app/src/test/java/lv/lu/finalwork/repository/ProductRepositoryTest.java
@@ -0,0 +1,24 @@
+package lv.lu.finalwork.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductRepositoryTest {
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
